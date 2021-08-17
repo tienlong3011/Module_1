747867fefa80a768c4c6ed3b9fba6162ef83f74d
unknown <email ng k git>
2021-08-17 10:01:08 +0700

    hien thi dau ra js file

---
+++ Javascript/hien thi dau ra.js
@@ -0,0 +1,10 @@
+function hienthidaura(){
+    // hàm alert()
+    // alert('xin chao !')
+    // sử dụng thẻ HTML
+    // document.getElementById('baitap' ).innerHTML="xin chào !"
+    // hàm document.write()
+    // document.write('hello bro !')
+    // hàm console.log()
+    // console.log('hello bro!')
+}
