b0473fe6611e59da650454c675654f7b127bef27
unknown <email ng k git>
2021-08-31 11:42:39 +0700

    bt

---
+++ Javascript/Hàm/Ứng dụng quản lý.js
@@ -0,0 +1,53 @@
+let productList = [];
+//ten ham: showAllProduct
+//ds tham so: khong co:
+//dl tra ve: khong co
+//chuc nang: lap danh sach san pham, dien vao trong the co id products
+function showAllProduct(){
+    let content="";
+    for (let i = 0; i < productList.length; i++) {
+        content += '<tr>\n' +
+            '            <td>'+productList[i]+'</td>\n' +
+            '            <td><button onclick="editProduct('+i+')">Edit</button></td>\n' +
+            '            <td><button onclick="deleteProduct('+i+')">Delete</button></td>\n' +
+            '        </tr>'
+    }
+    document.getElementById("Product").innerHTML = content;
+    document.getElementById("sumOfProduct").innerText = productList.length + " Product";
+}
+//ten ham: creatNewProduct
+//ds tham so: khong co:
+//dl tra ve: khong co
+//chuc nang:
+//Lấy dữ liệu
+// Push vào mảng
+// hiển thị lại dữ liệu
+function createNewProduct() {
+    // let newP = prompt("Moi ban nhap ten sp moi");
+    let newP = document.getElementById("newP").value;
+    productList.push(newP);
+    showAllProduct();
+    document.getElementById("newP").value = "";
+}
+//ten ham: deleteProduct
+//ds tham so: index
+//dl tra ve: khong co
+//chuc nang: xoa san pham theo index
+// hien thi lai danh sach
+function deleteProduct(index){
+    productList.splice(index, 1);
+    showAllProduct();
+}
+//ten ham: editProduct
+//ds tham so: index
+//dl tra ve: khong co
+//chuc nang: sua san pham theo index
+//1. cho nguoi dung nhap vao sp moi
+//2. gan lai sp cho sp cu
+//3. hien thi lai danh sach
+
+function editProduct(index){
+    let newValue = prompt("Sửa sản phẩm", productList[index]);
+    productList[index] = newValue;
+    showAllProduct();
+}
